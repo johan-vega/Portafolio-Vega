6b5e8c71bff0dd2b83e28dc7e8d21f46b70501e2
Andre Vega <johan.vi.em.99@gmail.com>
2026-05-25 16:31:33 -0500

    se corrigio error del banner

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,10 @@
+import { useState } from 'react';
 import Navbar from './components/Navbar/Navbar';
 import './App.css';
 import './index.css';
 import Banner from './components/Banner/Banner';
 import Perfil from './components/Perfil/Perfil';
+import Habilidades from './components/Habilidades/Habilidades';
 
 function App() {
   const [count, setCount] = useState(0)
@@ -12,6 +14,7 @@ function App() {
       <Navbar />
       <Banner />
       <Perfil />
+      <Habilidades />
       {/* El resto de secciones irán aquí debajo. Ejemplo: <Footer />*/
       }
 
